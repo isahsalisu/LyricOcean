3da1506c93c1c7e6e3709743eb12b882bbea93ff
IraLototska <67969648+IraLototska@users.noreply.github.com>
2023-02-19 00:57:59 +0200

    also added SearchPage.js
    Changed nav -> header.js

---
+++ src/components/tracks/Tracks.js
@@ -0,0 +1,63 @@
+// import React, {Component} from 'react';
+// import {Consumer} from '../../context';
+// import Spinner from '../layout/Spinner';
+// import Track from '../tracks/Track';
+
+// class Tracks extends Component {
+//     render(){
+//         return(
+//             <>
+//             <div>Hey</div>
+//             </>
+//             // <Consumer>
+//             //     {value => {
+//             //         const {track_list, heading} = value;
+//             //         if(track_list === undefined || track_list.length === 0){
+//             //             return <Spinner />;
+//             //         }else{
+//             //             return (
+//             //                 <React.Fragment>
+//             //                     <h3 style={{color: "#212529", fontWeight: "bold"}} className="text-center mb-4">{heading}</h3>
+//             //                 <div className="row">
+//             //                     {track_list.map(item => (
+//             //                         <Track key={item.track.track_id} track={item.track} />
+//             //                     ))}
+//             //                 </div>
+//             //                 </React.Fragment>
+//             //             );
+//             //         }
+//             //     }}
+//             // </Consumer>
+//         )
+//     }
+// }
+
+// export default Tracks;
+
+import Spinner from '../layout/Spinner';
+import Track from '../tracks/Track';
+
+const Tracks = (props) => {
+  return (
+    <>
+      <h3 style={{ color: "#212529", fontWeight: "bold" }} className="text-center mb-4">All Tracks</h3>
+      <div className="row">
+        {
+          props.allTracks
+          ? props.allTracks.map(item => (
+              <Track
+                key={item.track.track_id}
+                album={item.track.album_name}
+                artist={item.track.artist_name}
+                song={item.track.track_name}
+                url={item.track.track_share_url}
+              />
+            ))
+          : <Spinner />
+        }
+      </div>
+    </>
+  );
+};
+
+export default Tracks;
